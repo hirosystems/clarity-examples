90800060339f8a67600f3fbe1e2808b68ddda725
Max Efremov <51917427+mefrem@users.noreply.github.com>
2023-03-09 16:47:29 -0600

    NFT and FT contract examples

--- examples/fungible-token/contracts/fungible-token.clar
+++ examples/fungible-token/contracts/fungible-token.clar
@@ -1,43 +1,49 @@
-;; This contract implements the SIP010 community-standard Fungible Token trait.
+;; This contract implements the SIP-010 community-standard Fungible Token trait.
 (impl-trait 'SP3FBR2AGK5H9QBDH3EEN6DF8EK8JY7RX8QJ5SVTE.sip-010-trait-ft-standard.sip-010-trait)
 
 ;; Define the FT, with no maximum supply
 (define-fungible-token clarity-coin)
 
-;; Define constants for the transaction sender (deployer) and error codes.
-(define-constant CONTRACT_OWNER tx-sender)
+;; Define errors
 (define-constant ERR_OWNER_ONLY (err u100))
 (define-constant ERR_NOT_TOKEN_OWNER (err u101))
 
-;; Returns the balance of a specified principal.
+;; Define constants for contract
+(define-constant CONTRACT_OWNER tx-sender)
+(define-constant TOKEN_URI u"https://hiro.so") ;; utf-8 string with token metadata host
+(define-constant TOKEN_NAME "Clarity Coin")
+(define-constant TOKEN_SYMBOL "CC")
+(define-constant TOKEN_DECIMALS u6) ;; 6 units displayed past decimal, e.g. 1.000_000 = 1 token
+
+
+;; SIP-010 function: Get the token balance of a specified principal
 (define-read-only (get-balance (who principal))
   (ok (ft-get-balance clarity-coin who))
 )
 
-;; Returns the total supply of our custom token.
+;; SIP-010 function: Returns the total supply of fungible token
 (define-read-only (get-total-supply)
   (ok (ft-get-supply clarity-coin))
 )
 
-;; Returns a human-readable name for our token.
+;; SIP-010 function: Returns the human-readable token name
 (define-read-only (get-name)
-  (ok "Clarity Coin")
+  (ok TOKEN_NAME)
 )
 
-;; Returns the symbol or "ticker" for this token.
+;; SIP-010 function: Returns the symbol or "ticker" for this token
 (define-read-only (get-symbol)
-  (ok "CC")
+  (ok TOKEN_SYMBOL)
 )
 
-;; The number of decimals used for display. e.g. 6 means 1_000_000 represents 1 token
+;; SIP-010 function: Returns number of decimals to display
 (define-read-only (get-decimals)
-  (ok u6)
+  (ok TOKEN_DECIMALS)
 )
 
-;; Returns a link to a metadata file for the token.
-;; Our practice fungible token does not have a website, so we can return none.
+;; SIP-010 function: Returns the URI containing token metadata
 (define-read-only (get-token-uri)
-  (ok none)
+  (ok (some TOKEN_URI))
 )
 
 ;; Mint new tokens and send them to a recipient.
@@ -49,7 +55,7 @@
   )
 )
 
-;; Transfer tokens to a recipient.
+;; SIP-010 function: Transfers tokens to a recipient
 ;; Sender must be the same as the caller to prevent principals from transferring tokens they do not own.
 (define-public (transfer
   (amount uint)
--- examples/fungible-token/tests/fungible-token_test.ts
+++ examples/fungible-token/tests/fungible-token_test.ts
@@ -1,28 +1,88 @@
-
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.5/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
-
-// TODO
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types,
+} from "https://deno.land/x/clarinet@v1.0.5/index.ts";
+import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+  name: "Test deployer minting 100 tokens to 2nd party, checking balance.",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get("deployer");
+    let wallet_1 = accounts.get("wallet_1");
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "fungible-token",
+        "mint",
+        [
+          types.uint(100),
+          types.principal("ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5"),
+        ],
+        deployer.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    block.receipts[0].result.expectOk();
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "fungible-token",
+        "get-balance",
+        [types.principal("ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5")],
+        wallet_1.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 3);
+    block.receipts[0].result.expectOk().expectUint(100);
+  },
+});
+
+Clarinet.test({
+  name: "Test minting and transferring tokens.",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get("deployer");
+    let wallet_1 = accounts.get("wallet_1");
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "fungible-token",
+        "mint",
+        [
+          types.uint(100),
+          types.principal("ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5"),
+        ],
+        deployer.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    block.receipts[0].result.expectOk();
+
+    block = chain.mineBlock([
+      Tx.contractCall(
+        "fungible-token",
+        "transfer",
+        [
+          types.uint(42),
+          types.principal("ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5"),
+          types.principal("ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG"),
+          types.none(),
+        ],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        "fungible-token",
+        "get-balance",
+        [types.principal("ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG")],
+        wallet_1.address
+      ),
+    ]);
+    assertEquals(block.receipts.length, 2);
+    assertEquals(block.height, 3);
+    block.receipts[0].result.expectOk().expectTrue;
+    block.receipts[1].result.expectOk().expectUint(42);
+  },
 });
